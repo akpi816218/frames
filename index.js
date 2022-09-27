a95826b4dc88eac68044f9b23daaf5ffc3b61c2f
akpi816218 <111009970+akpi816218@users.noreply.github.com>
2022-09-27 12:04:33 -0700

    save

---
+++ index.js
@@ -0,0 +1,14 @@
+const q = document.querySelector;
+const isUrl = (s) => {
+  try {
+    let a = new URL(s);
+  } catch {
+    return false;
+  }
+  return true;
+};
+let f = Object.create(null);
+f.frame = document.createElement("iframe");
+f.frame.style =
+  "border: none; position: absolute; top:0; left: 0; height: 100vh; width: 100vw; z-index: 10000;";
+q("button").addEventListener("click", () => {});
